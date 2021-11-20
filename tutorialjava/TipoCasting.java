5d065e58b710709e5207856ce4b068884f6c53eb
edvaldoljr <edvaldoluizj@gmail.com>
2021-11-20 14:35:07 -0300

    Tipo Casting

---
+++ tutorialjava/TipoCasting.java
@@ -0,0 +1,73 @@
+package tutorialjava;
+
+public class TipoCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        System.out.println(b);
+
+        double c = 10.5;
+        int d = (int) c;
+        System.out.println(d);
+
+        int e = 10;
+        double f = e;
+        System.out.println(f);
+
+        double g = 10.5;
+        int h = (int) g;
+        System.out.println(h);
+
+        double i = 10.5;
+        int j = (int) i;
+        System.out.println(j);
+
+        double k = 10.5;
+        int l = (int) k;
+        System.out.println(l);
+
+        double m = 10.5;
+        int n = (int) m;
+        System.out.println(n);
+
+        double o = 10.5;
+        int p = (int) o;
+        System.out.println(p);
+
+        double q = 10.5;
+        int r = (int) q;
+        System.out.println(r);
+
+        double s = 10.5;
+        int t = (int) s;
+        System.out.println(t);
+
+        double u = 10.5;
+        int v = (int) u;
+        System.out.println(v);
+
+        double w = 10.5;
+        int x = (int) w;
+        System.out.println(x);
+
+        double y = 10.5;
+        int z = (int) y;
+        System.out.println(z);
+
+        double aa = 10.5;
+        int bb = (int) aa;
+        System.out.println(bb);
+
+        double cc = 10.5;
+        int dd = (int) cc;
+        System.out.println(dd);
+
+        double ee = 10.5;
+        int ff = (int) ee;
+        System.out.println(ff);
+
+        double gg = 10.5;
+        int hh = (int) gg;
+        System.out.println(hh);
+    }
+}

b7bcb32b26eb60fe629c615095f400dee3441bdb
edvaldoljr <edvaldoluizj@gmail.com>
2021-11-20 14:19:11 -0300

    Comentarios

---
+++ tutorialjava/Comentarios.java
@@ -0,0 +1,12 @@
+package tutorialjava;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentario de una linea
+        
+        /*
+        Comentario de varias lineas
+        */
+        System.out.println("Hola mundo");
+    }
+}

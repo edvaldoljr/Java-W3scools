3dbfcca8d4c577650b24e8f974fd12f677e69bf4
edvaldoljr <edvaldoluizj@gmail.com>
2021-11-20 14:23:30 -0300

    Tipo Primitivos

---
+++ tutorialjava/Tipos.java
@@ -0,0 +1,15 @@
+package tutorialjava;
+
+public class Tipos {
+    public static void main(String[] args) {
+        // Tipos primitivos
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+        float f = 3.14f;
+        double d = 3.14;
+        char c = 'a';
+        boolean bo = true;
+    }
+}
